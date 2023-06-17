a089b986fdd8fa620dd361cd23fe4345099389da
gamz007 <gsmbobdennis@gamail.com>
2023-06-17 06:29:15 -0700

    alx-low_level_programming

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <studio.h>
+#include <unistd.h>
+
+/**
+ * main Entry point 
+ * Retuen: Always 1 (success)
+ */
+int main (voi)
+{
+	write(2,
+	"and that piece of art is useful\" -Dora Korpar , 2015-10-19\n", 59);
+	retun (1);
+}
